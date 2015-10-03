bbfb07cdb0e8798ae1c6b2402677ecb3543300ca
Alec Nikolas Reiter <alecreiter@gmail.com>
2015-10-03 16:35:16 -0400

    Finish Angular app rough draft implementation
    Thoughts here:
    
    * Bulk loading Wiki title and summary together is really slow -- always
      has been. Perhaps separating this into two steps or a user driven
      event is better.
    
    * There's duplication here and error handling is really rough.

--- twiki/static/js/app.js
+++ twiki/static/js/app.js
@@ -26,17 +26,29 @@ function TweetService($http) {
 };
 
 
+function WikiPageService($http) {
+    var WikiPageService = {};
+
+    function pages(term) {
+        return $http.get('/pages/' + term);
+    };
+
+    WikiPageService.getPages = pages;
+    return WikiPageService
+}
+
 function TweetController($location, TweetService) {
-    vm = this;
+    var vm = this;
     vm.loaded = false;
     vm.error = false;
     vm.tweets = [];
 
     this.getTweets = function() {
         var term = $location.hash();
-        console.log('term is ' + term);
+        console.log('finding tweets');
         return TweetService.getTweets(term)
             .success(function(data, status, headers, config) {
+                console.log('loaded tweets');
                 vm.loaded = true;
                 vm.tweets = data.tweets.map(TweetService.provideLinkURL)
             })
@@ -48,10 +60,28 @@ function TweetController($location, TweetService) {
     };
 };
 
-function WikiController() {
-    this.getPages = function() {};
+function WikiController($location, WikiPageService) {
+    var vm = this;
+    vm.loaded = false;
+    vm.error = false;
+    vm.pages = []
+
+    this.getPages = function() {
+        var term = $location.hash();
+        return WikiPageService.getPages(term)
+            .success(function(data, status, headers, config) {
+                vm.pages = data.pages;
+                vm.loaded = true;
+            })
+            .error(function(data, status, headers, config) {
+                vm.loaded = true;
+                vm.error = true;
+                vm.msg = data.msg;
+            });
+    };
 };
 
 app.factory('TweetService', TweetService);
+app.factory('WikiPageService', WikiPageService);
 app.controller('TweetController', TweetController);
 app.controller('WikiController', WikiController);
